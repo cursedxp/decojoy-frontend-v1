c81b046bd63e5a9f70bc63440cbbeea8f9a0f7bb
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-12 18:13:57 +0200

    Check user session for "ADMIN" role

--- middleware/isAdmin.ts
+++ middleware/isAdmin.ts
@@ -5,14 +5,24 @@ async function isAdmin(
   req: IncomingMessage,
   res: ServerResponse
 ): Promise<boolean> {
+  // Attempt to retrieve the user's session information using the getSession function.
   const session = await getSession(req, res);
+
+  // Check if a session exists and if it contains user information.
   if (session && session.user) {
+    // Extract the user object from the session.
     const user = session.user;
+
+    // Check if the user has an "ADMIN" role in their roles array.
     if (user[process.env.AUTH0_API_URL + "roles"]?.includes("ADMIN")) {
+      // If the user has an "ADMIN" role, return true, indicating they are an admin.
       return true;
     }
   }
+
+  // If no valid session or "ADMIN" role is found, return false, indicating the user is not an admin.
   return false;
 }
 
+// Export the isAdmin function for use in other parts of the application.
 export default isAdmin;
