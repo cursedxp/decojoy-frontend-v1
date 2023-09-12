7427acde5f48bf98b37912eca82b16e2bbd2d2f5
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-12 18:15:44 +0200

    Add getServerSideProps function to handle server-side rendering and user authentication

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -6,18 +6,24 @@ const AdminContent: React.FC = () => {
   return <div>Admin-specific content here...</div>;
 };
 
+// Define a function called getServerSideProps to handle server-side rendering.
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Check if the current user is an admin by using the isAdmin middleware.
   const userIsAdmin = await isAdmin(context.req, context.res);
+
+  // If the user is not an admin, redirect them to an unauthorized page.
   if (!userIsAdmin) {
     return {
       redirect: {
-        destination: "/unauthorized",
-        permanent: false,
+        destination: "/unauthorized", // Redirect to the "/unauthorized" page.
+        permanent: false, // Indicate that this is not a permanent redirect.
       },
     };
   }
+
+  // If the user is an admin, continue rendering the page.
   return {
-    props: {},
+    props: {}, // No additional props are passed to the AdminContent component.
   };
 };
 
