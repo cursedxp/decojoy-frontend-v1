0de78fbe59d7004d0f68e517256ec038c824ee4c
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-13 17:00:14 +0200

    Add redux store in _app.tsx and configure it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "./layout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-
+import store from "../store";
 import "../src/app/styles/globals.css";
+import { Provider } from "react-redux";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </UserProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserProvider>
+    </Provider>
   );
 }
