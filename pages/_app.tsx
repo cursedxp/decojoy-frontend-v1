0092455b60a8abda4dbf068a7c0d4eede7cf0b87
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-17 13:28:34 +0200

    Add global CSS styles and remove unnecessary import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
+import "../src/app/styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "./layout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import store from "../store";
-import "../src/app/styles/globals.css";
 import { Provider } from "react-redux";
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
