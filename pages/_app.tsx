47d7baca80170a856d5c335fddf3417b36a5989d
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-18 19:25:31 +0200

    Fix import path for store in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "./layout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import store from "../store";
+import store from "../store/store";
 import { Provider } from "react-redux";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
