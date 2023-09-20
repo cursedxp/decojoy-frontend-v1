6ab90bc394a5cba228bee70f0436a46fe6d617ce
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-20 15:43:48 +0200

    Add ThumbnailSelector component to CreateConcept page

--- pages/admin/concepts/create/index.tsx
+++ pages/admin/concepts/create/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import ConceptImageUploader from "../../../../src/app/components/ConceptImageUploader";
 import { RootState } from "../../../../store/store";
+import ThumbnailSelector from "../../../../src/app/components/ThumbnailSelector";
+
 const CreateConcept: React.FC = () => {
   const images = useSelector((state: RootState) => state.concept.images); // Adjust the path according to your store structure
+
+  const [selectedThumbnail, setSelectedThumbnail] = useState<string | null>(
+    null
+  );
+
+  const handleThumbnailSelection = (imageUrl: string) => {
+    setSelectedThumbnail(imageUrl);
+  };
 
   return (
     <div>
       <h1>Create New Concept</h1>
       <ConceptImageUploader />
-      {/* Other components and elements */}
+      <ThumbnailSelector
+        uploadedImages={images}
+        onSelect={handleThumbnailSelection}
+      />
     </div>
   );
 };
