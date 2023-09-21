6d906bb507a2c18a51917e693f13202d4ca0313a
Anil Ozsoy <anilozsoy@gmail.com>
2023-09-21 20:36:50 +0200

    Update ConceptImageUploader component to manage imageFiles and previewImageUrls states. Add functionality to remove preview images when clicked

--- src/app/components/ConceptImageUploader.tsx
+++ src/app/components/ConceptImageUploader.tsx
@@ -1,138 +1,98 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
+import { PhotoIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { useDispatch } from "react-redux";
-import { setImages } from "../../../store/conceptSlice";
-import axios from "axios";
-import { PhotoIcon } from "@heroicons/react/24/outline";
 
+//TODO: upload images to servet and return image urls
+//TODO: return image urls to thumbnail selector component
 const ConceptImageUploader: React.FC = () => {
+  const [imageFiles, setImageFiles] = useState<File[]>([]);
+  const [previewImageUrls, setPreviewImageUrls] = useState<string[]>([]);
   const dispatch = useDispatch();
-  const [previewImagesURLs, setPreviewImagesURLs] = useState<string[]>([]);
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
 
+  //onDrop is a callback function that will be called when a file is dropped
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Add new files to the current list
-    setSelectedFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
+    //update imageFiles state
+    setImageFiles((prevImageFiles) => [...prevImageFiles, ...acceptedFiles]);
 
-    // Generate and add new previews to the current previews
-    const newPreviewsURLs = acceptedFiles.map((file) =>
-      URL.createObjectURL(file)
-    );
-    setPreviewImagesURLs((prevPreviewsURLs) => [
-      ...prevPreviewsURLs,
-      ...newPreviewsURLs,
+    //generate preview image urls
+    const newPreviewImageUrls = acceptedFiles.map((file: any) => {
+      return URL.createObjectURL(file);
+    });
+
+    //update previewImageUrls state
+    setPreviewImageUrls((prevPreviewImageUrls) => [
+      ...prevPreviewImageUrls,
+      ...newPreviewImageUrls,
     ]);
   }, []);
 
-  // Get access token for the current user
-  const getAccessToken = useCallback(async (): Promise<string> => {
-    const tokenResponse = await axios.get("/api/getAccessToken");
+  //remove preview image when user clicks on it
+  const removePreviewImage = (index: number) => {
+    //remove image from imageFiles state
+    const newImageFiles = imageFiles.filter((_, i) => i !== index);
+    setImageFiles(newImageFiles);
 
-    if (!tokenResponse.data.accessToken) {
-      throw new Error("Access token missing");
-    }
-
-    return tokenResponse.data.accessToken;
-  }, []);
-
-  // Upload images to the backend server
-  const uploadImagesToServer = useCallback(
-    async (accessToken: string): Promise<string[]> => {
-      const formData = new FormData();
-
-      selectedFiles.forEach((file) => formData.append("images", file));
-
-      const response = await axios.post(
-        process.env.NEXT_PUBLIC_API_URL + "/assets/upload",
-        formData,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      return response.data.data.imageUrls;
-    },
-    [selectedFiles]
-  );
-
-  // Clear the current selection of files and their previews
-  const clearSelection = useCallback(() => {
-    setPreviewImagesURLs([]);
-    setSelectedFiles([]);
-  }, []);
-
-  // Handle the image upload to the server
-  const handleUpload = useCallback(async () => {
-    try {
-      const accessToken = await getAccessToken(); // Get an access token
-      const imageUrls = await uploadImagesToServer(accessToken); // Upload images to the server
-
-      dispatch(setImages(imageUrls)); // Dispatch the uploaded image URLs to Redux store
-
-      // Clear previews and selected files after successful upload
-      clearSelection();
-    } catch (error) {
-      console.error("Error uploading images:", error);
-    }
-  }, [getAccessToken, uploadImagesToServer, dispatch, clearSelection]);
-
-  // Remove a specific file and its preview from the current selection
-  const removeFile = (index: number) => {
-    setPreviewImagesURLs((prev) => prev.filter((_, idx) => idx !== index));
-    setSelectedFiles((prev) => prev.filter((_, idx) => idx !== index));
+    //remove image from previewImageUrls state
+    const newPreviewImageUrls = previewImageUrls.filter((_, i) => i !== index);
+    setPreviewImageUrls(newPreviewImageUrls);
   };
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  useEffect(() => {
+    console.log(previewImageUrls);
+  }, [previewImageUrls]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div className="flex flex-col w-1/2 justify-center gap-4">
-      <div>
-        <h3 className="text-lg font-medium">Upload Concept Photos</h3>
+    <div className=" max-w-xl">
+      <div className=" my-4">
+        <h3 className="text-lg font-medium">Select concept images</h3>
         <p className=" text-sm text-gray-500">
-          Select files and click complete upload button to upload images to the
-          server
+          Upload images that best represent your concept
         </p>
       </div>
+
       <div
         {...getRootProps()}
-        className="flex flex-col items-center justify-center bg-gray-50 border border-gray-300 border-dashed p-4 rounded-xl cursor-pointer"
+        className="flex justify-center bg-gray-100 p-4 text-xs border-dashed border border-gray-300 rounded-xl"
       >
         <input {...getInputProps()} />
-        <div className="flex items-center gap-4">
-          <div className=" w-12 h-12 rounded-full flex justify-center items-center bg-gray-200">
-            <PhotoIcon className="h-6 w-6 text-gray-500" />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <div className="flex items-center gap-2">
+            <div className=" w-8 h-8 bg-gray-200 flex justify-center items-center rounded-full">
+              <PhotoIcon className="w-4 h-4 text-gray-500" />
+            </div>
+            <p>
+              <span className=" font-medium underline">Drag and drop</span>{" "}
+              files here, or click to select files
+            </p>
           </div>
-          <p className=" text-gray-500 text-sm text-center">
-            <span>Drag & drop</span> concept images here, or click to select
-            files
-          </p>
-        </div>
-      </div>
-      <div className="flex gap-4 flex-wrap">
-        {previewImagesURLs.map((src, index) => (
-          <img
-            key={index}
-            src={src}
-            alt="Preview"
-            className="mb-2 cursor-pointer rounded-xl"
-            width={80}
-            onClick={() => removeFile(index)} // Allow users to remove a selected image by clicking it
-            title="Click to remove"
-          />
-        ))}
-      </div>
-      <div className=" self-end">
-        {previewImagesURLs.length > 0 && (
-          <button
-            onClick={handleUpload} // Trigger the image upload process
-            className="flex gap-2 items-center py-2 px-4 bg-sky-600 rounded-xl text-white text-sm shadow-sm"
-          >
-            Complete Upload
-          </button>
         )}
+      </div>
+
+      <div className="my-4 flex gap-4">
+        {previewImageUrls.map((src, index) => (
+          <div key={index} className="relative group">
+            <img
+              src={src}
+              alt=""
+              className="rounded-xl  hover:opacity-80"
+              width={72}
+              onClick={() => removePreviewImage(index)}
+              title="Click to remove image"
+            />
+            {/* Initially hide the TrashIcon, and only show it when parent div is hovered */}
+            <div
+              className="absolute top-0 right-0 p-1 opacity-0 group-hover:opacity-100 bg-red-500 rounded-xl shadow-sm cursor-pointer"
+              onClick={() => removePreviewImage(index)}
+            >
+              <TrashIcon className="w-4 h-4 text-white" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
