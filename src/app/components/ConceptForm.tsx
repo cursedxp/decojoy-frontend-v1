c9d401fd79de8a9a1285d62357ff768f37244f04
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-02 11:17:52 +0200

    ✨ feat(ConceptForm.tsx): add ConceptForm component to handle concept details form submission 🐛 fix(ConceptForm.tsx): adjust initial value for thumbnail prop to handle null value correctly 🔒 chore(ConceptForm.tsx): add validation schema for the form fields to ensure data integrity and improve user experience

---
+++ src/app/components/ConceptForm.tsx
@@ -0,0 +1,248 @@
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const conceptStyles = [
+  "MINIMALIST",
+  "MODERN",
+  "INDUSTRIAL",
+  "SCANDINAVIAN",
+  "CONTEMPORARY",
+  "MIDCENTURY",
+  "ARTDECO",
+  "TRADITIONAL",
+  "BOHEMIAN",
+  "ECLECTIC",
+  "CLASSIC",
+];
+
+const roomTypes = [
+  "KITCHEN",
+  "LIVINGROOM",
+  "BEDROOM",
+  "DINING",
+  "BATHROOM",
+  "OFFICE",
+  "STUDY",
+  "ENTRYWAY",
+  "LIBRARY",
+  "NURSERY",
+  "KIDSROOM",
+];
+
+export interface CreateConceptFormData {
+  title: string;
+  description: string;
+  style: string;
+  roomType: string;
+  price: number;
+  images: string[];
+  thumbnail: string;
+}
+
+interface ConceptInfoFormProps {
+  thumbnail: string | null;
+  images: string[];
+}
+
+export interface ConceptFormRef {
+  getFormData: () => CreateConceptFormData;
+  submitForm: () => void;
+}
+// validation schema for the form
+const validationSchema = Yup.object({
+  title: Yup.string().required("Title is required"),
+  description: Yup.string().required("Description is required"),
+  price: Yup.number()
+    .typeError("Price must be a number")
+    .integer("Price must be an integer number")
+    .required("Price is required"),
+  Style: Yup.string().required("Concept style is required"),
+  roomType: Yup.string()
+    .oneOf(
+      roomTypes,
+      "Room type must be one of the following values: " + roomTypes.join(", ")
+    )
+    .required("Room type is required"),
+  thumbnail: Yup.string().required("Thumbnail is required"),
+  images: Yup.array()
+    .of(Yup.string().required("Each value in images must be a string"))
+    .required("Images must be an array"),
+});
+
+const ConceptForm = forwardRef<ConceptFormRef, ConceptInfoFormProps>(
+  (props, ref) => {
+    // formik hook for the form state management and validation handling logic
+    const formik = useFormik({
+      initialValues: {
+        title: "",
+        description: "",
+        price: 0,
+        roomType: "",
+        style: "",
+        thumbnail: props.thumbnail || "", // Adjusted this line
+        images: props.images || [],
+      },
+      validationSchema: validationSchema,
+      onSubmit: (values) => {},
+    });
+
+    // expose the formik methods to the parent component using the useImperativeHandle hook and the ref prop passed to the component
+    useImperativeHandle(ref, () => ({
+      getFormData: () => formik.values,
+      submitForm: formik.submitForm,
+      resetForm: formik.resetForm,
+    }));
+
+    return (
+      <div className="max-w-xl">
+        <div>
+          <h3 className="text-lg font-medium">Concept details </h3>
+          <p className="text-sm text-gray-500">
+            Enter details about your concept here and click submit to save it to
+          </p>
+        </div>
+        <div>
+          <form onSubmit={formik.handleSubmit} className="mt-4 flex gap-4">
+            <div className="flex flex-col">
+              <div>
+                <div>
+                  <label htmlFor="title" className="text-xs font-medium">
+                    Title
+                  </label>
+                  <p className=" text-xs text-gray-500">
+                    Enter your concept title here
+                  </p>
+                </div>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.title}
+                  className="block w-full border border-gray-300 rounded-md p-1 text-sm "
+                />
+                {formik.touched.title && formik.errors.title && (
+                  <p className="text-red-500 text-xs">{formik.errors.title}</p>
+                )}
+              </div>
+              <div>
+                <div>
+                  <label
+                    htmlFor="description"
+                    className=" text-xs font-medium mt-4"
+                  >
+                    Description
+                  </label>
+                  <p className=" text-xs text-gray-500">
+                    Enter your concept description here.
+                  </p>
+                </div>
+                <textarea
+                  id="description"
+                  name="description"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.description}
+                  className="block w-full border border-gray-300 rounded-md p-1 text-sm "
+                ></textarea>
+                {formik.touched.description && formik.errors.description && (
+                  <p className="text-red-500 text-xs">
+                    {formik.errors.description}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <div>
+                <div>
+                  <label htmlFor="style" className=" text-xs font-medium mt-4">
+                    Concept Style
+                  </label>
+                  <p className=" text-xs text-gray-500">
+                    Choose your concept style from the list
+                  </p>
+                </div>
+                <select
+                  id="style"
+                  name="style"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.style}
+                  className="block w-full border border-gray-300 rounded-md p-1 text-sm "
+                >
+                  {conceptStyles.map((style, index) => (
+                    <option key={index} value={style} className="capitalize">
+                      {style}
+                    </option>
+                  ))}
+                </select>
+                {formik.touched.style && formik.errors.style && (
+                  <p className="text-red-500 text-xs">{formik.errors.style}</p>
+                )}
+              </div>
+              <div>
+                <div>
+                  <label
+                    htmlFor="roomType"
+                    className=" text-xs font-medium mt-4"
+                  >
+                    Room Type
+                  </label>
+                  <p className=" text-xs text-gray-500">
+                    Choose your room type from the list
+                  </p>
+                </div>
+                <select
+                  id="roomType"
+                  name="roomType"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.roomType}
+                  className="block w-full border border-gray-300 rounded-md p-1 text-sm"
+                >
+                  {roomTypes.map((type, index) => (
+                    <option key={index} value={type} className="capitalize">
+                      {type}
+                    </option>
+                  ))}
+                </select>
+                {formik.touched.roomType && formik.errors.roomType && (
+                  <p className="text-red-500 text-xs">
+                    {formik.errors.roomType}
+                  </p>
+                )}
+              </div>
+              <div>
+                <div>
+                  <label htmlFor="price" className=" text-xs font-medium mt-4">
+                    Price
+                  </label>
+                  <p className=" text-xs text-gray-500">
+                    Enter your concept price here
+                  </p>
+                </div>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.price}
+                  className="block w-full border border-gray-300 rounded-md p-1 text-sm"
+                />
+                {formik.touched.price && formik.errors.price && (
+                  <p className="text-red-500 text-xs">{formik.errors.price}</p>
+                )}
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+);
+
+ConceptForm.displayName = "ConceptForm";
+export default ConceptForm;
