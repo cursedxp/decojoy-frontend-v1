61b1d588e060477a7ded92e6acbbc80cb405fc16
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-07 21:00:10 +0200

    📝 chore(products): create ProductsPage component to display a list of products and allow creating new products

---
+++ pages/admin/products/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import Modal from "@/app/components/Modal";
+import { ToastContainer, toast } from "react-toastify";
+import CustomTable from "@/app/components/CustomTable";
+import ProductForm from "@/app/components/ProductForm";
+
+const ProductsPage: React.FC = () => {
+  const [showModal, setShowModal] = React.useState(false);
+  const onClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <main className="p-16 flex-col h-screen  bg-stone-100">
+      <div className=" text-xs text-gray-500 uppercase ">
+        Admin / <span className=" text-sky-500 font-semibold">Products</span>
+      </div>
+      <div className="flex justify-between items-center my-4">
+        <div className="flex-col">
+          <h1 className="text-lg font-medium">Products</h1>
+          <p className=" text-sm text-gray-500">
+            A list of all products in the database. Click on a product to see
+            product details.
+          </p>
+        </div>
+        <button
+          className=" flex gap-2 items-center py-2 px-4 bg-sky-600 rounded-xl text-white text-sm shadow-sm"
+          onClick={() => setShowModal(true)}
+        >
+          <PlusCircleIcon className="h-5 w-5" />
+          Create
+        </button>
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Modal onClose={onClose} isOpen={showModal}>
+        <ProductForm />
+      </Modal>
+    </main>
+  );
+};
+
+export default ProductsPage;
