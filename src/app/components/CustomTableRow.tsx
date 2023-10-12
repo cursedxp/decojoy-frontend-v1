eacc4340138c32bb6a3366284c75d3b755604dbf
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-12 19:07:21 +0200

    ✨ feat(CustomTableRow.tsx): add CustomTableRow component to display a row of data in a custom table
    The CustomTableRow component is added to display a row of data in a custom table. It receives an item object as a prop, which contains various properties such as id, title, description, thumbnail, price, style, status, type, createdAt, imageUrl, category, and url.
    
    The component also receives optional onDelete, onPublish, and onUnPublish callbacks as props, which can be used to handle delete, publish, and unpublish actions respectively.
    
    The component includes logic to format the date using the formatDate function, which converts the date string to a localized date format.
    
    The component renders the item's thumbnail, title, description, type, style, price, createdAt, and status. The status is displayed as a colored dot, with green indicating "published" and orange indicating "unpublished".
    
    The component also includes commented out code for delete and publish/unpublish buttons, which can be uncommented and used with the respective callbacks to handle those actions.

---
+++ src/app/components/CustomTableRow.tsx
@@ -0,0 +1,117 @@
+import React, { useCallback } from "react";
+import Image from "next/image";
+import { on } from "events";
+interface CustomTableProps {
+  item: {
+    id: number;
+    title: string;
+    description?: string;
+    thumbnail?: string;
+
+    price: number;
+    style: string;
+    status?: string;
+    type: string;
+    createdAt: string;
+    imageUrl?: string;
+    category?: string;
+    url?: string;
+  };
+
+  onDelete?: (id: number) => void;
+  onPublish?: (id: number) => void;
+  onUnPublish?: (id: number) => void;
+}
+const CustomTableRow: React.FC<CustomTableProps> = ({
+  item,
+  onDelete,
+  onPublish,
+  onUnPublish,
+}) => {
+  const formatDate = useCallback((date: string) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString("eu-EU", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+  }, []);
+
+  return (
+    <tr>
+      {item.thumbnail && (
+        <td className="py-3 px-6 border-gray-300">
+          <Image
+            src={item.thumbnail}
+            alt="thumbnail"
+            width={56}
+            height={56}
+            className=" rounded-md"
+          />
+        </td>
+      )}
+      <td className="py-3 px-6  border-gray-300">{item.title}</td>
+      {item.description && (
+        <td className="py-3 px-6  border-gray-300">{item.description}</td>
+      )}
+      {item.type && (
+        <td className="py-3 px-6  border-gray-300">
+          {item.type.charAt(0).toUpperCase() + item.type.slice(1).toLowerCase()}
+        </td>
+      )}
+      {item.style && (
+        <td className="py-3 px-6  border-gray-300">
+          {item.style.charAt(0).toUpperCase() +
+            item.style.slice(1).toLowerCase()}
+        </td>
+      )}
+      {item.price && (
+        <td className="py-3 px-6  border-gray-300">{item.price}</td>
+      )}
+
+      {item.createdAt && (
+        <td className="py-3 px-6  border-gray-300">
+          {formatDate(item.createdAt)}
+        </td>
+      )}
+      {item.status && (
+        <td className="py-3 px-6 border-gray-300">
+          <div
+            className={
+              (item.status.toLowerCase() === "published"
+                ? "bg-green-300 rounded-xl "
+                : "bg-orange-300 rounded-xl ") + "w-4 h-4"
+            }
+          ></div>
+        </td>
+      )}
+
+      <td className="py-3 px-6  border-gray-300">
+        {/* <div className="flex gap-2">
+        <button
+          className=" hover:bg-red-100  text-red-500 font-bold p-2 rounded-xl"
+          onClick={() => onDelete(item.id)}
+        >
+          <TrashIcon className="h-5 w-5" />
+        </button>
+        {item.status === "PUBLISHED" ? (
+          <button
+            className="  hover:bg-orange-100 text-orange-400 p-2 rounded-xl"
+            onClick={() => onUnPublish(item.id)}
+          >
+            <EyeSlashIcon className="h-5 w-5" />
+          </button>
+        ) : (
+          <button
+            className="  hover:bg-green-100 text-green-700 p-2 rounded-xl"
+            onClick={() => onPublish(item.id)}
+          >
+            <EyeIcon className="h-5 w-5" />
+          </button>
+        )}
+      </div> */}
+      </td>
+    </tr>
+  );
+};
+export default CustomTableRow;
