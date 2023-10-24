61d30e28a358ea61ee936edbf6408163e5221dc6
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-24 16:17:18 +0200

    🔥 refactor(CustomTableActions.tsx): remove unused component and its dependencies to improve code cleanliness and reduce bundle size 🔨 refactor(CustomTableRow.tsx): rename concept prop to item for better clarity and consistency 🔨 refactor(CustomTableRow.tsx): remove commented out code for CustomTableActions component as it is no longer used ✨ feat(CustomTableRow.tsx): replace CustomTableActions component with inline JSX to simplify code and improve performance

--- src/app/components/CustomTableRow.tsx
+++ src/app/components/CustomTableRow.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback } from "react";
 import Image from "next/image";
 import { TrashIcon, EyeSlashIcon, EyeIcon } from "@heroicons/react/24/outline";
-import CustomTableActions from "./CustomTableActions";
+import { on } from "events";
 interface CustomTableProps {
-  concept: {
+  item: {
     thumbnail?: string;
     title?: string;
     description?: string;
@@ -14,9 +14,17 @@ interface CustomTableProps {
     status?: string;
     id?: string;
   };
+  onRemove?: (id: string | undefined) => void;
+  onPublish?: (id: string | undefined) => void;
+  onUnPublish?: (id: string | undefined) => void;
 }
 
-const CustomTableRow: React.FC<CustomTableProps> = ({ concept }) => {
+const CustomTableRow: React.FC<CustomTableProps> = ({
+  item,
+  onRemove,
+  onPublish,
+  onUnPublish,
+}) => {
   const formatDate = useCallback((date: string) => {
     const newDate = new Date(date);
     return newDate.toLocaleDateString("eu-EU", {
@@ -28,9 +36,9 @@ const CustomTableRow: React.FC<CustomTableProps> = ({ concept }) => {
   return (
     <tr>
       <td className="py-3 px-6 border-gray-300">
-        {concept.thumbnail && (
+        {item.thumbnail && (
           <Image
-            src={concept.thumbnail}
+            src={item.thumbnail}
             alt="thumbnail"
             width={56}
             height={56}
@@ -39,45 +47,77 @@ const CustomTableRow: React.FC<CustomTableProps> = ({ concept }) => {
         )}
       </td>
 
-      <td className="py-3 px-6  border-gray-300">{concept.title}</td>
-      {concept.description && (
-        <td className="py-3 px-6  border-gray-300">{concept.description}</td>
+      <td className="py-3 px-6  border-gray-300">{item.title}</td>
+      {item.description && (
+        <td className="py-3 px-6  border-gray-300">{item.description}</td>
       )}
-      {concept.type && (
+      {item.type && (
         <td className="py-3 px-6  border-gray-300">
-          {concept.type.charAt(0).toUpperCase() +
-            concept.type.slice(1).toLowerCase()}
+          {item.type.charAt(0).toUpperCase() + item.type.slice(1).toLowerCase()}
         </td>
       )}
-      {concept.style && (
+      {item.style && (
         <td className="py-3 px-6  border-gray-300">
-          {concept.style.charAt(0).toUpperCase() +
-            concept.style.slice(1).toLowerCase()}
+          {item.style.charAt(0).toUpperCase() +
+            item.style.slice(1).toLowerCase()}
         </td>
       )}
-      {concept.price && (
-        <td className="py-3 px-6  border-gray-300">{concept.price}</td>
+      {item.price && (
+        <td className="py-3 px-6  border-gray-300">{item.price}</td>
       )}
 
-      {concept.createdAt && (
+      {item.createdAt && (
         <td className="py-3 px-6  border-gray-300">
-          {formatDate(concept.createdAt)}
+          {formatDate(item.createdAt)}
         </td>
       )}
-      {concept.status && (
+      {item.status && (
         <td className="py-3 px-6 border-gray-300">
           <div
             className={
-              (concept.status.toLowerCase() === "published"
+              (item.status.toLowerCase() === "published"
                 ? "bg-green-300 rounded-xl "
                 : "bg-orange-300 rounded-xl ") + "w-4 h-4"
             }
           ></div>
         </td>
       )}
-
       <td className="py-3 px-6  border-gray-300">
-        {/* <CustomTableActions conceptId={concept.id} status={concept.status} /> */}
+        <div className="flex gap-2">
+          <button
+            className=" hover:bg-red-100  text-red-500 font-bold p-2 rounded-xl"
+            onClick={() => {
+              if (onRemove) {
+                onRemove(item.id);
+              }
+            }}
+          >
+            <TrashIcon className="h-5 w-5" />
+          </button>
+          {item.status === "PUBLISHED" ? (
+            <button
+              className="hover:bg-orange-100 text-orange-400 p-2 rounded-xl"
+              onClick={() => {
+                if (onUnPublish) {
+                  onUnPublish(item.id);
+                }
+              }}
+            >
+              <EyeSlashIcon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              className="hover:bg-green-100 text-green-700 p-2 rounded-xl"
+              onClick={() => {
+                if (onPublish) {
+                  onPublish(item.id);
+                }
+              }}
+            >
+              <EyeIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
       </td>
     </tr>
   );
