792a4314788327ba9ccd2ad761598f08f463f691
Anil Ozsoy <anilozsoy@gmail.com>
2023-10-25 17:44:22 +0200

    ✨ feat(useDeleteRequest.ts): add custom hook for making DELETE requests to an API
    This commit adds a new file `useDeleteRequest.ts` which contains a custom hook `useDeleteRequest`. This hook is used for making DELETE requests to an API. It takes an `apiUrl` parameter and returns an object with `response`, `error`, `isLoading`, and `sendDeleteRequest` properties.
    
    The `sendDeleteRequest` function sends a DELETE request to the specified `apiUrl` with the provided `id`. It handles errors and sets the `response` and `error` states accordingly. It also sets the `isLoading` state to `true` while the request is being made and sets it back to `false` when the request is completed.
    
    This hook relies on the `useHeaders` hook to retrieve the necessary headers for the request. If the headers are missing or incorrect, an error is set in the `error` state.
    
    This custom hook can be used to easily make DELETE requests in React components.

---
+++ src/app/hooks/useDeleteRequest.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import useHeaders from "./useHeaders";
+import axios from "axios";
+
+const useDeleteRequest = (apiUrl: string) => {
+  const [response, setResponse] = useState<any[]>([]);
+  const [error, setError] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [headers, headersError] = useHeaders("application/json");
+
+  const sendDeleteRequest = async (id: number) => {
+    if (!headers) {
+      setError({
+        message: "Headers are missing or incorrect.",
+        details: headersError,
+      });
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await axios.delete(`${apiUrl}/${id}`, {
+        headers: headers.headers,
+      });
+      setResponse(response.data);
+    } catch (apiError) {
+      setError({
+        message: "API call failed.",
+        details: apiError,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return { response, error, isLoading, sendDeleteRequest };
+};
+export default useDeleteRequest;
