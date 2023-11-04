46ab1a10ecd84ca8c8b8c27d1800cdf88e525020
Anil Ozsoy <anilozsoy@gmail.com>
2023-11-04 15:11:08 +0100

    ✨ feat(useToastNotifications.ts): add custom hook for toast notifications
    This commit adds a new file `useToastNotifications.ts` which contains a custom hook `useToastNotification`. This hook provides two functions `notifySuccess` and `notifyError` for displaying toast notifications using the `react-toastify` library. The notifications are styled with a light theme and positioned at the top-right corner of the screen. They automatically close after 3000 milliseconds and have various other options for customization.

---
+++ src/app/hooks/useToastNotifications.ts
@@ -0,0 +1,31 @@
+import { toast } from "react-toastify";
+
+const useToastNotification = () => {
+  const notifySuccess = (message: string) =>
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
+  const notifyError = (message: string) => {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "light",
+    });
+  };
+
+  return { notifySuccess, notifyError };
+};
+
+export default useToastNotification;
