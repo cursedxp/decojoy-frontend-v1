dc776de4b44409df6b5cc07b5c791406b624c66b
Anil Ozsoy <anilozsoy@gmail.com>
2023-11-05 17:42:23 +0100

    🐛 fix(usePostRequests.ts): fix typo in import statement for React library ✨ feat(usePostRequests.ts): refactor usePostRequests hook to use a state object for response, error, and readiness to improve readability and maintainability 🔧 chore(usePostRequests.ts): add comments to clarify the logic for handling queued requests and waiting for headers to be ready

--- src/app/hooks/usePostRequests.ts
+++ src/app/hooks/usePostRequests.ts
@@ -1,41 +1,59 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, use } from "react";
 import axios from "axios";
 import useHeaders from "./useHeaders";
 
-const usePostRequests = (data: any, apiUrl: string) => {
-  const [response, setResponse] = useState<any[]>([]);
-  const [requestError, setError] = useState<any>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+interface PostRequestState {
+  response: any;
+  error: any;
+  isReady: boolean;
+}
+
+const usePostRequests = (id: any, apiUrl: string) => {
+  const [state, setState] = useState<PostRequestState>({
+    response: null,
+    error: null,
+    isReady: false,
+  });
+
   const {
     headers,
     isReady,
     error: headersError,
   } = useHeaders("application/json");
 
+  const requestQueued = React.useRef(false);
+
   const sendPostRequest = useCallback(
-    async (requestData = data) => {
-      if (!headers) {
-        setError({
-          message: "Headers are missing or incorrect.",
-          details: headersError,
-        });
+    async (requestData = id) => {
+      // If headers are not ready, queue the request and wait.
+      if (!isReady) {
+        requestQueued.current = true;
+        setState((prevState) => ({ ...prevState, isReady: false }));
         return;
       }
 
-      setIsLoading(true);
+      // If headers are ready and the request is queued, proceed with the request.
+      setState((prevState) => ({ ...prevState, isReady: true }));
+
       try {
-        const response = await axios.post(apiUrl, requestData, headers);
-        setResponse(response.data);
-      } catch (apiError) {
-        setError({ message: "API call failed.", details: apiError });
-      } finally {
-        setIsLoading(false);
+        const res = await axios.post(apiUrl, requestData, { headers: headers });
+        setState((prevState) => ({ ...prevState, response: res.data }));
+      } catch (error) {
+        setState((prevState) => ({ ...prevState, error: error }));
       }
     },
-    [apiUrl, headers, headersError, data]
+    [apiUrl, headers, isReady, id]
   );
 
-  return { response, requestError, isLoading, sendPostRequest };
+  // Watch for headers to become ready if there's a queued request.
+  useEffect(() => {
+    if (isReady && requestQueued.current) {
+      sendPostRequest();
+      requestQueued.current = false;
+    }
+  }, [sendPostRequest, isReady]);
+
+  return { state, sendPostRequest };
 };
 
 export default usePostRequests;
